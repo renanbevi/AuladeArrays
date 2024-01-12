df5f00a7ae2988b9b3f12d2300e737a80b14c3b1
Nuvemenvio <renanbevi@gmail.com>
2024-01-12 17:00:35 -0300

    Aulas de ArrayList Alura

---
+++ src/Principal.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+
+
+        Pessoa renan = new Pessoa();
+        renan.setNome("Renanzin");
+        renan.setIdade(33);
+
+        Pessoa juliane = new Pessoa();
+        juliane.setNome("Juliane");
+        juliane.setIdade(33);
+
+        Pessoa luca =  new Pessoa();
+        luca.setNome("Luca");
+        luca.setIdade(1);
+
+
+        ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
+        listaDePessoas.add(renan);
+        listaDePessoas.add(juliane);
+        listaDePessoas.add(luca);
+
+
+        System.out.println("Tamanho da Lista: " + listaDePessoas.size());
+        System.out.println("Primeira pessoa: " + listaDePessoas.get(0));
+        System.out.println("Lista Completa: " + listaDePessoas);
+
+
+
+
+
+
+    }
+}
